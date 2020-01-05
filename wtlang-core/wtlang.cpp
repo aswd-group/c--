e16f8b2ae1acebdb3c077244c140cea2d463a9d0
Andy <hackingservers@163.com>
2020-01-05 21:51:52 +0800

    Add wtlang

---
+++ wtlang-core/wtlang.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <windows.h>
+
+using namespace std;
+
+class wtl {
+	public:
+		
+};
+
+int main() {
+	
+	string fileName, inName, outName;
+	
+	cin >> fileName;
+	cout << "\nCompiling...\n";
+	
+	inName = fileName.find(".wtl") ? fileName : fileName + ".wtl";
+	outName = 
+	freopen(fileName + ".wtl", "r", stdin);
+	freopen("." + fileName + ".cpp", "w", stdin);
+	
+	sort(a, a + n, [](int a, int b){
+		cmp;
+	});
+	
+	return 0;
+	
+}
+
